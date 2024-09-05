0f3ff326ccb16ced5d20a78d6c84bb6291569e16
Sean Sylver <ssylver@VividSolutions.com>
2024-09-05 10:00:17 -0700

    Remove database folder

--- database/scripts/01_00_00/00/ddl/roles/wfprev.ddl.create_roles.sql
+++
@@ -1,4 +0,0 @@
-CREATE ROLE "app_wf1_prev_custodian";
-
-CREATE ROLE "app_wf1_prev_rest_proxy";
-GRANT "app_wf1_prev_rest_proxy" TO "proxy_wf1_prev_rest";
--- database/scripts/01_00_00/00/ddl/schema/wfprev.ddl.create_wfprev_schema.sql
+++
@@ -1,9 +0,0 @@
--- SCHEMA: wfprev
-
--- DROP SCHEMA "wfprev" ;
-
-CREATE SCHEMA "wfprev";
-
-GRANT ALL ON SCHEMA "wfprev" TO "app_wf1_prev";
-
-GRANT USAGE ON SCHEMA "wfprev" TO "app_wf1_prev_rest_proxy";
