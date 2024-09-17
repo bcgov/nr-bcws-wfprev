ace67b13275b389dfb7a96791abc5e1ea5db6c14
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2024-09-17 14:31:26 -0700

    Test scan (#37)
    * update unit test

--- client/wfprev-war/src/main/angular/src/app/app.component.spec.ts
+++ client/wfprev-war/src/main/angular/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('wfprev app is running!');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wfprev app is running!');
-  });
 });
