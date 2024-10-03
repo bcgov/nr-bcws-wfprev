2c43307f8161e19ed638323126f7ad29fc680e13
Chris Preston <cpreston@vividsolutions.com>
2024-10-03 10:50:53 -0700

    Add additional security group config to resolve networking issue

--- terraform/security.tf
+++ terraform/security.tf
@@ -9,3 +9,16 @@ data "aws_security_group" "app" {
 data "aws_security_group" "data" {
   name = "Data_sg"
 }
+
+resource "aws_security_group" "wfprev_tomcat_access" {
+  name = "wfprev-ecs-tasks-allow-access"
+  description = "Explicitly allow traffic on ports used by WFPREV"
+  vpc_id = module.network.aws_vpc.id
+
+  ingress {
+    protocol = "tcp"
+    from_port = var.WFPREV_CLIENT_PORT
+    to_port = var.WFPREV_API_PORT
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
