b7ce8964f02bf73b104d6956904bbcc68b724fe2
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2024-10-04 10:15:19 -0700

    Update health check path

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -126,7 +126,7 @@ resource "aws_alb_target_group" "wfprev_ui" {
     protocol            = "HTTP"
     matcher             = "200"
     timeout             = "3"
-    path                = "/${aws_apigatewayv2_stage.wfprev_stage.name}/wfprev-ui/"
+    path                = "/${aws_apigatewayv2_stage.wfprev_stage.name}/${var.PREVENTION_WAR_NAMES[0]}/"
     unhealthy_threshold = "2"
   }
 }
