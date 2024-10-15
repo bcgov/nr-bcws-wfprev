d115fbd0e2e5524c2bf32f1ebccdbef5ad67f087
Sean Sylver <ssylver@VividSolutions.com>
2024-10-15 13:12:45 -0700

    Update autoscale.tf resource ID, remove nginx scaling

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -1,14 +1,6 @@
 resource "aws_appautoscaling_target" "wfprev_target" {
    service_namespace  = "ecs"
-   resource_id        = "service/${aws_ecs_cluster.wfprev_main.name}/${aws_ecs_service.wfprev_main.name}"
-   scalable_dimension = "ecs:service:DesiredCount"
-   min_capacity       = 1
-   max_capacity       = 10
-}
-
-resource "aws_appautoscaling_target" "wfprev_nginx_target" {
-   service_namespace  = "ecs"
-   resource_id        = "service/${aws_ecs_cluster.wfprev_main.name}/${aws_ecs_service.nginx.name}"
+   resource_id        = "service/${aws_ecs_cluster.wfprev_main.name}/${aws_ecs_service.wfprev_server.name}"
    scalable_dimension = "ecs:service:DesiredCount"
    min_capacity       = 1
    max_capacity       = 10
@@ -35,26 +27,6 @@ resource "aws_appautoscaling_policy" "wfprev_up" {
    depends_on = [aws_appautoscaling_target.wfprev_target]
 }
 
-resource "aws_appautoscaling_policy" "wfprev_nginx_up" {
-   name               = "wfprev_nginx_scale_up"
-   service_namespace  = "ecs"
-   resource_id        = "service/${aws_ecs_cluster.wfprev_main.name}/${aws_ecs_service.nginx.name}"
-   scalable_dimension = "ecs:service:DesiredCount"
-
-   step_scaling_policy_configuration {
-     adjustment_type         = "ChangeInCapacity"
-     cooldown                = 60
-     metric_aggregation_type = "Maximum"
-
-     step_adjustment {
-       metric_interval_lower_bound = 0
-       scaling_adjustment          = 1
-     }
-   }
-
-   depends_on = [aws_appautoscaling_target.wfprev_nginx_target]
-}
-
 # Automatically scale capacity down by one
 resource "aws_appautoscaling_policy" "wfprev_down" {
    name               = "wfprev_scale_down"
@@ -74,26 +46,6 @@ resource "aws_appautoscaling_policy" "wfprev_down" {
    }
 
    depends_on = [aws_appautoscaling_target.wfprev_target]
-}
-
-resource "aws_appautoscaling_policy" "wfprev_nginx_down" {
-   name               = "wfprev_nginx_scale_down"
-   service_namespace  = "ecs"
-   resource_id        = "service/${aws_ecs_cluster.wfprev_main.name}/${aws_ecs_service.nginx.name}"
-   scalable_dimension = "ecs:service:DesiredCount"
-
-   step_scaling_policy_configuration {
-     adjustment_type         = "ChangeInCapacity"
-     cooldown                = 60
-     metric_aggregation_type = "Maximum"
-
-     step_adjustment {
-       metric_interval_upper_bound = 0
-       scaling_adjustment          = -1
-     }
-   }
-
-   depends_on = [aws_appautoscaling_target.wfprev_nginx_target]
 }
 
 # CloudWatch alarm that triggers the autoscaling up policy
@@ -120,28 +72,6 @@ resource "aws_cloudwatch_metric_alarm" "wfprev_service_cpu_high" {
    }
 }
 
-resource "aws_cloudwatch_metric_alarm" "wfprev_nginx_service_cpu_high" {
-   alarm_name          = "wfprev_nginx_cpu_utilization_high"
-   comparison_operator = "GreaterThanOrEqualToThreshold"
-   evaluation_periods  = "1"
-   metric_name         = "CPUUtilization"
-   namespace           = "AWS/ECS"
-   period              = "60"
-   statistic           = "Average"
-   threshold           = "50"
-
-   dimensions = {
-     ClusterName = aws_ecs_cluster.wfprev_main.name
-     ServiceName = aws_ecs_service.nginx.name
-   }
-
-   alarm_actions = [aws_appautoscaling_policy.wfprev_nginx_up.arn]
-
-  tags = {
-     Environment = "${var.TARGET_ENV}"
-   }
-}
-
 # CloudWatch alarm that triggers the autoscaling down policy
 resource "aws_cloudwatch_metric_alarm" "wfprev_service_cpu_low" {
    alarm_name          = "wfprev_cpu_utilization_low"
@@ -160,29 +90,6 @@ resource "aws_cloudwatch_metric_alarm" "wfprev_service_cpu_low" {
 
    alarm_actions = [aws_appautoscaling_policy.wfprev_down.arn]
 
-   tags = {
-     Environment = "${var.TARGET_ENV}"
-   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "wfprev_nginx_service_cpu_low" {
-   alarm_name          = "wfprev_nginx_cpu_utilization_low"
-   comparison_operator = "LessThanOrEqualToThreshold"
-   evaluation_periods  = "2"
-   metric_name         = "CPUUtilization"
-   namespace           = "AWS/ECS"
-   period              = "60"
-   statistic           = "Average"
-   threshold           = "10"
-
-   dimensions = {
-     ClusterName = aws_ecs_cluster.wfprev_main.name
-     ServiceName = aws_ecs_service.nginx.name
-   }
-
-   alarm_actions = [aws_appautoscaling_policy.wfprev_nginx_down.arn]
-
-  
    tags = {
      Environment = "${var.TARGET_ENV}"
    }
