3c814265dfde244060f623be354fe3bfd80c5916
Lucas Li <yzlucas@outlook.com>
2024-10-16 13:35:53 -0700

    config breakpoints in component insetad of html hardcod

--- client/wfprev-war/src/main/angular/src/app/components/resizable-panel/resizable-panel.component.ts
+++ client/wfprev-war/src/main/angular/src/app/components/resizable-panel/resizable-panel.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resizable-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resizable-panel.component.html',
   styleUrl: './resizable-panel.component.scss'
 })
 export class ResizablePanelComponent {
   panelWidth: string = '50vw';  // Default panel width at 50%
+  breakpoints = [5, 50, 90];
   @Output() panelResized = new EventEmitter<void>();
+  
   // Method to resize the panel based on given percentage
   resizePanel(percentage: number): void {
     this.panelWidth = `${percentage}vw`;  // Resize based on viewport width
