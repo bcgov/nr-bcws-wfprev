6f2850cba8aec9c5f2a0ad1bf2997237a007be8e
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2024-10-17 17:21:48 -0700

    Deploying Angular Node.js Application Using Docker (#209)
    * wip
    
    * Deploying Angular Node.js Application Using Docker via express
    
    * Ignore node_modules in backend and remove from tracking

---
+++ client/wfprev-war/backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Host setup
+const port = process.env.PORT || 8080;
+const hostname = "0.0.0.0";
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Serve static files from the Angular app
+app.use(express.static(path.join(__dirname, 'dist/wfprev')));
+
+// Send all requests to Angular app
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/wfprev/index.html'));
+});
+
+const server = http.createServer(app);
+
+// Listen on {port} and {hostname} to be accessible from public IP address
+server.listen(port, hostname, () => {
+    console.log(`angular app running on http://${hostname}:${port}`);
+});
