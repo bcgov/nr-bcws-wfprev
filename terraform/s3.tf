31e011d4eefed9aab0f168d885792c55e0bdd8e0
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2024-11-19 15:28:18 -0800

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,59 +9,6 @@ resource "aws_s3_bucket" "wfprev_site_bucket" {
   }
 }
 
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for WFPREV site."
-}
-
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Distribution for WFPREV site."
-  default_root_object = "index.html"
-
-  origin {
-    domain_name = aws_s3_bucket.wfprev_site_bucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.wfprev_site_bucket.bucket
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-    }
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.wfprev_site_bucket.bucket
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  tags = {
-    Name = "wfprev-distribution"
-  }
-}
-
 # Uploading assets. This shouldn't be needed because we'll push them up from the 
 # github action, vs having terraform fetch them
 #resource "aws_s3_object" "upload-assets" {
