2efcd3ab3f636d859778e7732773f5fa8acad700
Sean Sylver <ssylver@VividSolutions.com>
2024-11-19 11:21:02 -0800

    Access iam user created in DynamoDB and Add GitHub OIDC role for secure AWS access in GitHub Actions workflows

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -65,16 +65,22 @@ resource "aws_iam_role_policy" "wfprev_ecs_task_execution_cwlogs" {
 EOF
 }
 
-# Retrieve the parameter value from AWS Parameter Store
+# Retrieve the parameter value from AWS Systems Manager (SSM) Parameter Store
+# This fetches the SecureString parameter containing the GitHub Actions user's keys.
+# The parameter's value will be used later for accessing AWS resources securely.
 data "aws_ssm_parameter" "github_actions_user_keys" {
   name = "/iam_users/wfprev_github_actions_user_keys"
 }
 
-# IAM User for GitHub Actions (with limited permissions - Cloudfront invalidate, bucket cleanup)
+# Create an IAM user specifically for GitHub Actions
+# This user will be granted limited permissions for performing specific tasks (e.g., S3 operations and CloudFront invalidations).
 resource "aws_iam_user" "github_actions_user" {
   name = "github_actions_user"
 }
 
+
+# Define an IAM policy to allow access to the SSM parameter
+# This policy grants permissions to retrieve the specified SecureString parameter.
 resource "aws_iam_policy" "ssm_parameter_access" {
   name        = "SSMParameterAccess"
   description = "Allows access to SecureString parameters in SSM Parameter Store"
@@ -95,11 +101,17 @@ resource "aws_iam_policy" "ssm_parameter_access" {
   })
 }
 
+# Attach the SSM parameter access policy to the GitHub Actions IAM user
+# This links the user with the necessary permissions to read the SSM parameter securely.
 resource "aws_iam_user_policy_attachment" "ssm_parameter_access_attachment" {
   user       = aws_iam_user.github_actions_user.name
   policy_arn = aws_iam_policy.ssm_parameter_access.arn
 }
 
+# Define an IAM policy for GitHub Actions user to perform specific operations
+# This policy grants permissions to:
+# - Upload/delete objects in an S3 bucket
+# - Invalidate cached content in CloudFront
 resource "aws_iam_user_policy" "github_actions_policy" {
   name = "github-actions-policy"
   user = data.aws_ssm_parameter.github_actions_user_keys.name
@@ -121,8 +133,26 @@ resource "aws_iam_user_policy" "github_actions_policy" {
   })
 }
 
+# Create an IAM role for GitHub Actions to assume
 resource "aws_iam_role" "github_actions_role" {
-  name               = "github-actions-role"
-  assume_role_policy = data.aws_iam_user_policy.github_actions_policy.json
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::$${var.TARGET_AWS_ACCOUNT_ID}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:bcgov/nr-bcws-wfprev:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
 }
 
