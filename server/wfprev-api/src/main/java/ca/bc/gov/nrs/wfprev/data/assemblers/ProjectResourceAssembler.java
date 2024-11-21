debb3018ba984639a03f4a6e5dcc29398206ac8a
Dylan Hemsworth <dylan.hemsworth@gmail.com>
2024-11-21 14:00:57 -0800

    Add project boundary, geometry configs

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/assemblers/ProjectResourceAssembler.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/assemblers/ProjectResourceAssembler.java
@@ -1,4 +1,5 @@
 package ca.bc.gov.nrs.wfprev.data.assemblers;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
