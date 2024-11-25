baff22686c1bc8c2a17a72958e995225a952bb2b
Sean Sylver <ssylver@VividSolutions.com>
2024-11-25 10:25:30 -0800

    Provider for github actions OIDC

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -119,23 +119,29 @@ resource "aws_iam_user_policy" "github_actions_policy" {
   })
 }
 
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
 # Create an IAM role for GitHub Actions to assume
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::${var.TARGET_AWS_ACCOUNT_ID}:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:sub" = "repo:bcgov/nr-bcws-wfprev:*"
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "${aws_iam_openid_connect_provider.github_actions.url}:aud" : "sts.amazonaws.com"
+            "${aws_iam_openid_connect_provider.github_actions.url}:sub" : "repo:bcgov/nr-bcws-wfprev:*"
           }
         }
       }
