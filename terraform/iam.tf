d77052ab90bfee1dc79db013463e6338bd7bf047
Sean Sylver <ssylver@VividSolutions.com>
2024-11-27 16:09:18 -0800

    Hardcode ARN for existing provider

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -129,7 +129,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github_actions.arn
+          Federated = "arn:aws:iam::183631341627:oidc-provider/token.actions.githubusercontent.com"
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
