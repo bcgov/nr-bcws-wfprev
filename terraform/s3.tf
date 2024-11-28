d63fd381b756e4eba09ee57f55c4d70ec62694da
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2024-11-28 08:59:30 -0800

    Wfprev 30 test build 2 (#299)

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -32,20 +32,6 @@ resource "aws_s3_bucket_policy" "wfprev_site_bucket_policy" {
           "AWS" : "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
         },
         Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.wfprev_site_bucket.arn}/*"
-      },
-      resource "aws_s3_bucket_policy" "wfprev_site_bucket_policy" {
-  bucket = aws_s3_bucket.wfprev_site_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          "AWS" : "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
-        },
-        Action   = "s3:GetObject",
         Resource = "arn:aws:s3:::wfprev-dev-site/*"
       },
       {
@@ -64,9 +50,6 @@ resource "aws_s3_bucket_policy" "wfprev_site_bucket_policy" {
           "arn:aws:s3:::wfprev-dev-site/*"
         ]
       }
-    ]
-  })
-}
     ]
   })
 }
