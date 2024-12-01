4d04fbc9554a27818acedd2229d0fdf9232df907
Aidan Gordon <agordon@vividsolutions.com>
2024-12-01 11:56:48 -0800

    Added CodeServicesTest

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/services/CodesService.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/services/CodesService.java
@@ -25,75 +25,81 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class CodesService implements CommonService {
-  private ForestAreaCodeRepository forestAreaCodeRepository;
-  private ForestAreaCodeResourceAssembler forestAreaCodeResourceAssembler;
-  private GeneralScopeCodeRepository generalScopeCodeRepository;
-  private GeneralScopeCodeResourceAssembler generalScopeCodeResourceAssembler;
-  private ProjectTypeCodeRepository projectTypeCodeRepository;
-  private ProjectTypeCodeResourceAssembler projectTypeCodeResourceAssembler;
+    private ForestAreaCodeRepository forestAreaCodeRepository;
+    private ForestAreaCodeResourceAssembler forestAreaCodeResourceAssembler;
+    private GeneralScopeCodeRepository generalScopeCodeRepository;
+    private GeneralScopeCodeResourceAssembler generalScopeCodeResourceAssembler;
+    private ProjectTypeCodeRepository projectTypeCodeRepository;
+    private ProjectTypeCodeResourceAssembler projectTypeCodeResourceAssembler;
 
-  public CodesService(ForestAreaCodeRepository forestAreaCodeRepository, ForestAreaCodeResourceAssembler forestAreaCodeResourceAssembler,
-                      GeneralScopeCodeRepository generalScopeCodeRepository, GeneralScopeCodeResourceAssembler generalScopeCodeResourceAssembler,
-                      ProjectTypeCodeRepository projectTypeCodeRepository, ProjectTypeCodeResourceAssembler projectTypeCodeResourceAssembler) {
-    this.forestAreaCodeRepository = forestAreaCodeRepository;
-    this.forestAreaCodeResourceAssembler = forestAreaCodeResourceAssembler;
-    this.generalScopeCodeRepository = generalScopeCodeRepository;
-    this.generalScopeCodeResourceAssembler = generalScopeCodeResourceAssembler;
-    this.projectTypeCodeRepository = projectTypeCodeRepository;
-    this.projectTypeCodeResourceAssembler = projectTypeCodeResourceAssembler;
-  }
+    public CodesService(ForestAreaCodeRepository forestAreaCodeRepository, ForestAreaCodeResourceAssembler forestAreaCodeResourceAssembler,
+                        GeneralScopeCodeRepository generalScopeCodeRepository, GeneralScopeCodeResourceAssembler generalScopeCodeResourceAssembler,
+                        ProjectTypeCodeRepository projectTypeCodeRepository, ProjectTypeCodeResourceAssembler projectTypeCodeResourceAssembler) {
+        this.forestAreaCodeRepository = forestAreaCodeRepository;
+        this.forestAreaCodeResourceAssembler = forestAreaCodeResourceAssembler;
+        this.generalScopeCodeRepository = generalScopeCodeRepository;
+        this.generalScopeCodeResourceAssembler = generalScopeCodeResourceAssembler;
+        this.projectTypeCodeRepository = projectTypeCodeRepository;
+        this.projectTypeCodeResourceAssembler = projectTypeCodeResourceAssembler;
+    }
 
-  /** FOREST AREA CODES **/
-  public CollectionModel<ForestAreaCodeModel> getAllForestAreaCodes() throws ServiceException {
-    try {
-      List<ForestAreaCodeEntity> entities = new ArrayList<>();
-      return forestAreaCodeResourceAssembler.toCollectionModel(entities);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
+    /**
+     * FOREST AREA CODES
+     **/
+    public CollectionModel<ForestAreaCodeModel> getAllForestAreaCodes() throws ServiceException {
+        try {
+            List<ForestAreaCodeEntity> entities = forestAreaCodeRepository.findAll();
+            return forestAreaCodeResourceAssembler.toCollectionModel(entities);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
     }
-  }
-  
-  public ForestAreaCodeModel getForestAreaCodeById(String id) throws ServiceException {
-    try {
-        return forestAreaCodeRepository.findById(id).map(forestAreaCodeResourceAssembler::toModel).orElse(null);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
-    }
-  }
 
-  /** GENERAL SCOPE CODES **/
-  public CollectionModel<GeneralScopeCodeModel> getAllGeneralScopeCodes() throws ServiceException {
-    try {
-      List<GeneralScopeCodeEntity> entities = new ArrayList<>();
-      return generalScopeCodeResourceAssembler.toCollectionModel(entities);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
+    public ForestAreaCodeModel getForestAreaCodeById(String id) throws ServiceException {
+        try {
+            return forestAreaCodeRepository.findById(id).map(forestAreaCodeResourceAssembler::toModel).orElse(null);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
     }
-  }
-  
-  public GeneralScopeCodeModel getGeneralScopeCodeById(String id) throws ServiceException {
-    try {
-        return generalScopeCodeRepository.findById(id).map(generalScopeCodeResourceAssembler::toModel).orElse(null);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
-    }
-  }
 
-  /** PROJECT TYPE CODES **/
-  public CollectionModel<ProjectTypeCodeModel> getAllProjectTypeCodes() throws ServiceException {
-    try {
-      List<ProjectTypeCodeEntity> entities = new ArrayList<>();
-      return projectTypeCodeResourceAssembler.toCollectionModel(entities);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
+    /**
+     * GENERAL SCOPE CODES
+     **/
+    public CollectionModel<GeneralScopeCodeModel> getAllGeneralScopeCodes() throws ServiceException {
+        try {
+            List<GeneralScopeCodeEntity> entities = generalScopeCodeRepository.findAll();
+            return generalScopeCodeResourceAssembler.toCollectionModel(entities);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
     }
-  }
-  
-  public ProjectTypeCodeModel getProjectTypeCodeById(String id) throws ServiceException {
-    try {
-        return projectTypeCodeRepository.findById(id).map(projectTypeCodeResourceAssembler::toModel).orElse(null);
-    } catch(Exception e) {
-      throw new ServiceException(e.getLocalizedMessage(), e);
+
+    public GeneralScopeCodeModel getGeneralScopeCodeById(String id) throws ServiceException {
+        try {
+            return generalScopeCodeRepository.findById(id).map(generalScopeCodeResourceAssembler::toModel).orElse(null);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
     }
-  }
+
+    /**
+     * PROJECT TYPE CODES
+     **/
+    public CollectionModel<ProjectTypeCodeModel> getAllProjectTypeCodes() throws ServiceException {
+        try {
+            List<ProjectTypeCodeEntity> entities = projectTypeCodeRepository.findAll();
+            return projectTypeCodeResourceAssembler.toCollectionModel(entities);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
+    }
+
+    public ProjectTypeCodeModel getProjectTypeCodeById(String id) throws ServiceException {
+        try {
+            return projectTypeCodeRepository.findById(id).map(projectTypeCodeResourceAssembler::toModel).orElse(null);
+        } catch (Exception e) {
+            throw new ServiceException(e.getLocalizedMessage(), e);
+        }
+    }
 }
