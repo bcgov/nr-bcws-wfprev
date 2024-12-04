0fcc5dc81a51876fc27b284220ca22229b4e3561
Sean Sylver <ssylver@VividSolutions.com>
2024-12-03 17:40:48 -0800

    Latest app.module.spec.ts

--- client/wfprev-war/src/main/angular/src/app/app.module.spec.ts
+++ client/wfprev-war/src/main/angular/src/app/app.module.spec.ts
@@ -2,16 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OAuthLogger } from 'angular-oauth2-oidc';
 import { AppModule } from './app.module';
 import { ErrorPageComponent } from './components/error-page/error-page/error-page.component';
 import { CoreUIModule } from './lib/core-ui.module';
-import { CustomOAuthLogger } from './utils';
 import { LibraryConfig } from './config/library-config';
 import { AppConfigService } from './services/app-config.service';
 import { APP_INITIALIZER } from '@angular/core';
 import { TokenService } from './services/token.service';
-
+ 
 describe('AppModule', () => {
   let mockAppConfigService: any;
   let mockTokenService: any;
@@ -53,11 +51,6 @@ describe('AppModule', () => {
   it('should import CoreUIModule with correct configuration', () => {
     const coreModule = TestBed.inject(CoreUIModule);
     expect(coreModule).toBeTruthy();
-  });
-
-  it('should provide CustomOAuthLogger for OAuthLogger', () => {
-    const oauthLogger = TestBed.inject(OAuthLogger);
-    expect(oauthLogger instanceof CustomOAuthLogger).toBeTrue();
   });
 
   it('should declare ErrorPageComponent', () => {
