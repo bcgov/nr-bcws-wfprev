977332056a6cb66ebbfc5d1084e9873012b6dbd5
Henry Saldyga <hsaldyga@vividsolutions.com>
2024-12-27 15:11:40 -0800

    WFPREV-181

---
+++ db/scripts/01_00_04/00/ddl/WFPREV.alter_project.sql
@@ -0,0 +1,11 @@
+
+ALTER TABLE "wfprev"."project" ADD  COLUMN
+	"primary_funding_source_guid" UUID NULL;    -- primary_funding_source: Is a foreign key to funding_source: Funding Source is a list of agencies/organizations that are sources of project funding. The funding source provides the budget for the project. Example Funding Sources:  	- WRR - Wildfire Risk Reduction 	- BCP - BC Parks 	- FEP - Forest Employment Program 	- FESBC - Forest Enhancement Society of BC 	- CFS - FireSmart Community Funding Supports   Potential Secondary Funding Sources  	- CLWRR - Crown Land Wildfire Risk Reduction 	- Forest District Org Unit 	- Community Resiliency Investment 	- ER - Ecosytem Restoration
+
+COMMENT ON COLUMN "wfprev"."project"."primary_funding_source_guid"
+	IS 'primary_funding_source_guid: Is a foreign key to funding_source: Funding Source is a list of agencies/organizations that are sources of project funding. The funding source provides the budget for the project. Example Funding Sources:  	- WRR - Wildfire Risk Reduction 	- BCP - BC Parks 	- FEP - Forest Employment Program 	- FESBC - Forest Enhancement Society of BC 	- CFS - FireSmart Community Funding Supports   Potential Secondary Funding Sources  	- CLWRR - Crown Land Wildfire Risk Reduction 	- Forest District Org Unit 	- Community Resiliency Investment 	- ER - Ecosytem Restoration';
+
+CREATE INDEX "prjct_fndsrc_primary_idx" ON "wfprev"."project" ("primary_funding_source_guid" ASC);
+
+ALTER TABLE "wfprev"."project" ADD CONSTRAINT "prjct_fndsrc_primary_fk"
+	FOREIGN KEY ("funding_source_guid") REFERENCES "wfprev"."funding_source" ("primary_funding_source_guid") ON DELETE No Action ON UPDATE No Action;
