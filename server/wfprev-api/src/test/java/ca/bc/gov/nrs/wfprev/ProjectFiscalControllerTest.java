fbdad8b16d175bc800ff8b9ab07041b168d048a2
Aidan Gordon <agordon@vividsolutions.com>
2025-01-01 22:06:57 -0800

    WFPREV-199 Adding more test coverage for project fiscals.

--- server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/ProjectFiscalControllerTest.java
+++ server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/ProjectFiscalControllerTest.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.nrs.wfprev;
 
+import ca.bc.gov.nrs.wfone.common.service.api.ServiceException;
 import ca.bc.gov.nrs.wfprev.controllers.ProjectFiscalController;
 import ca.bc.gov.nrs.wfprev.data.models.ProjectFiscalModel;
 import ca.bc.gov.nrs.wfprev.services.ProjectFiscalService;
@@ -20,10 +21,8 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -138,5 +137,139 @@ class ProjectFiscalControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.projectPlanFiscalGuid").value(inputModel.getProjectPlanFiscalGuid()));
+    }
+
+    @Test
+    @WithMockUser
+    void testGetAllProjectFiscals_Success() throws Exception {
+        // GIVEN a list of ProjectFiscalModel
+        List<ProjectFiscalModel> projectFiscalModels = List.of(
+                ProjectFiscalModel.builder()
+                        .projectPlanFiscalGuid("123e4567-e89b-12d3-a456-426614174000")
+                        .projectGuid("123e4567-e89b-12d3-a456-426614174001")
+                        .activityCategoryCode("Tactical Planning")
+                        .fiscalYear(2024L)
+                        .submissionTimestamp(new Date(1672531200000L))
+                        .build(),
+                ProjectFiscalModel.builder()
+                        .projectPlanFiscalGuid("223e4567-e89b-12d3-a456-426614174002")
+                        .projectGuid("223e4567-e89b-12d3-a456-426614174003")
+                        .activityCategoryCode("Strategic Planning")
+                        .fiscalYear(2025L)
+                        .submissionTimestamp(new Date(1672627600000L))
+                        .build()
+        );
+
+        when(projectFiscalService.getAllProjectFiscals()).thenReturn(CollectionModel.of(projectFiscalModels));
+
+        // WHEN we call the getAllProjectFiscals method
+        mockMvc.perform(get("/projectFiscals")
+                        .contentType(MediaType.APPLICATION_JSON))
+                // THEN we expect a 200 OK response
+                .andExpect(status().isOk())
+                // AND validate the entire response structure for the first projectFiscalModel
+                .andExpect(jsonPath("$._embedded.projectFiscals[0].projectPlanFiscalGuid")
+                        .value("123e4567-e89b-12d3-a456-426614174000"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[0].projectGuid")
+                        .value("123e4567-e89b-12d3-a456-426614174001"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[0].activityCategoryCode")
+                        .value("Tactical Planning"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[0].fiscalYear")
+                        .value(2024))
+                .andExpect(jsonPath("$._embedded.projectFiscals[0].submissionTimestamp")
+                        .value(1672531200000L))
+                // AND validate the entire response structure for the second projectFiscalModel
+                .andExpect(jsonPath("$._embedded.projectFiscals[1].projectPlanFiscalGuid")
+                        .value("223e4567-e89b-12d3-a456-426614174002"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[1].projectGuid")
+                        .value("223e4567-e89b-12d3-a456-426614174003"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[1].activityCategoryCode")
+                        .value("Strategic Planning"))
+                .andExpect(jsonPath("$._embedded.projectFiscals[1].fiscalYear")
+                        .value(2025))
+                .andExpect(jsonPath("$._embedded.projectFiscals[1].submissionTimestamp")
+                        .value(1672627600000L));
+    }
+
+    @Test
+    @WithMockUser
+    void testGetAllProjectFiscals_ServiceException() throws Exception {
+        // GIVEN the service throws a ServiceException
+        when(projectFiscalService.getAllProjectFiscals()).thenThrow(new ServiceException("Test ServiceException"));
+        // WHEN we call the getAllProjectFiscals method
+        mockMvc.perform(get("/projectFiscals")
+                        .contentType(MediaType.APPLICATION_JSON))
+                // THEN we expect a 500 Internal Server Error
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$").doesNotExist()); // Ensure no body is returned
+    }
+
+    @Test
+    @WithMockUser
+    void testGetAllProjectFiscals_Empty() throws Exception {
+        // GIVEN an empty list from the service
+        when(projectFiscalService.getAllProjectFiscals()).thenReturn(CollectionModel.of(Collections.emptyList()));
+
+        // WHEN we call the getAllProjectFiscals method
+        mockMvc.perform(get("/projectFiscals")
+                        .contentType(MediaType.APPLICATION_JSON))
+                // THEN we expect a 200 OK response
+                .andExpect(status().isOk())
+                // AND the response body should have no projectFiscals
+                .andExpect(jsonPath("$._embedded").doesNotExist());
+    }
+
+    @Test
+    @WithMockUser
+    void testGetAllProjectFiscals_UnexpectedException() throws Exception {
+        // GIVEN the service throws a RuntimeException
+        when(projectFiscalService.getAllProjectFiscals()).thenThrow(new RuntimeException("Unexpected error"));
+
+        // WHEN we call the getAllProjectFiscals method
+        mockMvc.perform(get("/projectFiscals")
+                        .contentType(MediaType.APPLICATION_JSON))
+                // THEN we expect a 500 Internal Server Error
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$").doesNotExist()); // Ensure no body is returned
+    }
+
+    @Test
+    @WithMockUser
+    void testCreateProjectFiscal_Success() throws Exception {
+        // GIVEN a ProjectFiscalModel
+        ProjectFiscalModel inputModel = ProjectFiscalModel.builder()
+                .projectPlanFiscalGuid("123e4567-e89b-12d3-a456-426614174000")
+                .projectGuid("123e4567-e89b-12d3-a456-426614174001")
+                .activityCategoryCode("Tactical Planning")
+                .fiscalYear(2024L)
+                .submissionTimestamp(new Date(1672531200000L))
+                .build();
+
+        // WHEN we call the createProjectFiscal method
+        when(projectFiscalService.createProjectFiscal(inputModel)).thenReturn(inputModel);
+
+        // THEN we expect a 201 Created response
+        // AND the response body should match the input model
+        mockMvc.perform(MockMvcRequestBuilders.post("/projectFiscals")
+                        .content(gson.toJson(inputModel))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.projectPlanFiscalGuid").value(inputModel.getProjectPlanFiscalGuid()));
+    }
+
+    @Test
+    @WithMockUser
+    void testCreateProjectFiscal_ServiceException() throws Exception {
+        // GIVEN I am creating a new ProjectFiscalModel
+        // WHEN the service throws a ServiceException
+        when(projectFiscalService.createProjectFiscal(new ProjectFiscalModel()))
+                .thenThrow(new ServiceException("Test ServiceException"));
+        // THEN I expect a 500 Internal Server Error
+        // AND the response body should be empty
+        mockMvc.perform(MockMvcRequestBuilders.post("/projectFiscals")
+                        .content(gson.toJson(new ProjectFiscalModel()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$").doesNotExist());
     }
 }
