5895792631533da440ac2b1b722bf1437fba869d
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2025-01-06 09:38:03 -0800

    Api hookup (#395) (#396)

---
+++ client/wfprev-war/src/main/angular/src/app/services/project-services.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, map, Observable,throwError } from "rxjs";
+import { AppConfigService } from "src/app/services/app-config.service";
+import { TokenService } from "src/app/services/token.service";
+
+@Injectable({
+    providedIn: 'root',
+  })
+
+export class ProjectService {
+    constructor(
+        private readonly appConfigService: AppConfigService,
+        private readonly httpClient: HttpClient,
+        private readonly tokenService: TokenService,
+    ){
+    }
+
+    fetchProjects(): Observable<any> {
+        const url = `${this.appConfigService.getConfig().rest['wfprev']
+        }/wfprev-api/projects`;
+    
+        return this.httpClient.get(
+            url, {
+                headers: {
+                    Authorization: `Bearer ${this.tokenService.getOauthToken()}`,
+                }
+            }
+        ).pipe(
+            map((response: any) => response),
+            catchError((error) => {
+                console.error("Error fetching projects", error);
+                return throwError(() => new Error("Failed to fetch projects"));
+            })
+        );
+    }
+
+    createProject(project: any): Observable<any> {
+        const url = `${this.appConfigService.getConfig().rest['wfprev']}/wfprev-api/projects`;
+        return this.httpClient.post<any>(
+            url,
+            project,
+            {
+                headers: {
+                    Authorization: `Bearer ${this.tokenService.getOauthToken()}`,
+                }
+            }
+        );
+    }
+}
