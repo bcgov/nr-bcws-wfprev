9fc743aa794c7c296430564b209d0dcc05911c73
Henry Saldyga <hsaldyga@vividsolutions.com>
2025-01-22 11:29:16 -0800

    Feature/wfprev 206 (#436)

---
+++ db/scripts/01_00_09/00/dml/app_wf1_prev.dml.reporting_period_code.sql
@@ -0,0 +1,14 @@
+
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('DAILY', 'Daily', 1, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('MONTHLY', 'Monthly', 2, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('Q1', 'First Quarter', 3, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('Q2', 'Second Quarter', 4, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('Q3', 'Third Quarter', 5, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+insert into wfprev.reporting_period_code (reporting_period_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('Q4', 'Fourth Quarter', 6, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-01-2025', 'dd-mm-yyyy'));
+
