e979f6002d0866004fa3d2e7dcb5baf31030f9d6
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2025-01-31 15:25:43 -0800

    WFPREV-107 Fix ancillaryFundingSourceGuid and only return fiscals belonging to project (#478)

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/repositories/ProjectFiscalRepository.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/repositories/ProjectFiscalRepository.java
@@ -4,8 +4,10 @@ import ca.bc.gov.nrs.wfprev.common.repository.CommonRepository;
 import ca.bc.gov.nrs.wfprev.data.entities.ProjectFiscalEntity;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(exported = false)
 public interface ProjectFiscalRepository extends CommonRepository<ProjectFiscalEntity, UUID> {
+    List<ProjectFiscalEntity> findAllByProject_ProjectGuid(UUID projectGuid);
 }
