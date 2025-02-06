25562c5e2800051cbe063ae8578ac89dfb6d4e85
Sean Sylver <ssylver@VividSolutions.com>
2025-02-05 16:04:25 -0800

    re-add converter

---
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/common/converters/BooleanConverter.java
@@ -0,0 +1,32 @@
+package ca.bc.gov.nrs.wfprev.common.converters;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+/**
+ * Example of a converter for attributes
+ */
+
+@Converter
+public class BooleanConverter implements AttributeConverter<Boolean, String> {
+
+    /**
+     * Convert Boolean object to a String
+     * Stored in DB as VARCHAR(1) 'Y' or 'N'
+     */
+    @Override
+    public String convertToDatabaseColumn(Boolean convert) {
+        if (convert != null) {
+            return convert ? "Y" : "N";
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String convert) {
+        if (convert != null) {
+            return convert.equalsIgnoreCase("Y");
+        }
+        return null;
+    }
+}
