e80a8b9faa56920b9d1fe397144253a246f34f5c
Henry Saldyga <hsaldyga@vividsolutions.com>
2025-02-24 13:26:23 -0800

    Feature/wfprev 310 (#499)

---
+++ db/scripts/01_00_13/00/dml/app_wf1_prev.dml.funding_stream.sql
@@ -0,0 +1,8 @@
+INSERT INTO wfprev.funding_stream(
+	funding_stream_guid, funding_stream, revision_count, create_user, create_date, update_user, update_date)
+	VALUES ('2242d84a-c911-4a22-9582-e0f90857e7cf', 'Fuel Management', 0, 'DATA_LOAD', to_date('02-14-2025', 'mm-dd-yyyy'), 'DATA_LOAD', to_date('02-14-2025', 'mm-dd-yyyy'));
+
+INSERT INTO wfprev.funding_stream(
+	funding_stream_guid, funding_stream, revision_count, create_user, create_date, update_user, update_date)
+	VALUES ('1b0c1ab1-c047-47ed-acc4-b8dacfb0c691', 'Cultural & Prescribed Fire', 0, 'DATA_LOAD', to_date('02-14-2025', 'mm-dd-yyyy'), 'DATA_LOAD', to_date('02-14-2025', 'mm-dd-yyyy'));
+
