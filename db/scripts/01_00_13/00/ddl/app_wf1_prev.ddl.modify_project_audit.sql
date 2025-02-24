036adafc9558c1de6dbbe5059f3654835f425538
Henry Saldyga <hsaldyga@vividsolutions.com>
2025-02-24 12:02:40 -0800

    WFPREV-310

--- db/scripts/01_00_13/00/ddl/app_wf1_prev.ddl.modify_project_audit.sql
+++ db/scripts/01_00_13/00/ddl/app_wf1_prev.ddl.modify_project_audit.sql
@@ -1,6 +1,6 @@
-ALTER TABLE wfprev.project_audit DROP COLUMN PRIMARY_FUNDING_SOURCE;
+ALTER TABLE wfprev.project_audit DROP COLUMN PRIMARY_FUNDING_SOURCE_GUID;
 
-ALTER TABLE wfprev.project_audit ADD COLUMN "funding_stream_guid" UUID NULL;
-COMMENT ON COLUMN "wfprev"."project_audit"."funding_stream_guid"
+ALTER TABLE wfprev.project_audit ADD COLUMN funding_stream_guid UUID NULL;
+COMMENT ON COLUMN wfprev.project_audit.funding_stream_guid
 	IS 'funding_stream_guid: Is a foreign key to funding_stream: Funding Stream defines the funding streams that provides monies to fund projects. Values are:  	- Fuels Management 	- Cultural and Prescribed Burn.   Note: The funding streams currently align 1 to 1 with project type, but this can change in the future to a larger set of funding streams.'
 ;
