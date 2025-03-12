fb395be30cebedfdc8a75213b10b125fa36fadfc
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2025-03-12 15:51:24 -0700

    WFPREV-264 Update projectBoundaries endpoint (#524)

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/repositories/ProjectBoundaryRepository.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/repositories/ProjectBoundaryRepository.java
@@ -1,10 +1,20 @@
 package ca.bc.gov.nrs.wfprev.data.repositories;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ca.bc.gov.nrs.wfprev.common.repository.CommonRepository;
 import ca.bc.gov.nrs.wfprev.data.entities.ProjectBoundaryEntity;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RepositoryRestResource(exported = false)
 public interface ProjectBoundaryRepository extends CommonRepository<ProjectBoundaryEntity, String> {
-  
+
+    List<ProjectBoundaryEntity> findByProjectGuid(@NotNull UUID projectGuid);
+
+    Optional<ProjectBoundaryEntity> findByProjectBoundaryGuid(@NotNull UUID id);
+
+    void deleteByProjectBoundaryGuid(@NotNull UUID id);
 }
