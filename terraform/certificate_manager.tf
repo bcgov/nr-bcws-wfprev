04fe650ec3f88b211bd64626e2dc57f2a8f0cbb9
Dylan <dylan.hemsworth@gmail.com>
2025-03-31 09:42:45 -0700

    Update certificate_manager.tf (#551)

--- terraform/certificate_manager.tf
+++ terraform/certificate_manager.tf
@@ -1,19 +1,19 @@
 //NOTE: US certificate is needed for cloudfront
+// This process fails after 1 hour, dns entry for the cname record doesn't seem to take?
+//resource "aws_acm_certificate" "wfprev_domain_certificate" {
+//  domain_name       = data.aws_route53_zone.wfprev_route53_zone.name
+//  validation_method = "DNS"
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
 
-resource "aws_acm_certificate" "wfprev_domain_certificate" {
-  domain_name       = data.aws_route53_zone.wfprev_route53_zone.name
-  validation_method = "DNS"
+//  provider = aws.aws-us
+//}
 
-  lifecycle {
-    create_before_destroy = true
-  }
+//resource "aws_acm_certificate_validation" "domain_certificate_validation" {
+//  certificate_arn         = aws_acm_certificate.wfprev_domain_certificate.arn
+//  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
 
-  provider = aws.aws-us
-}
-
-resource "aws_acm_certificate_validation" "domain_certificate_validation" {
-  certificate_arn         = aws_acm_certificate.wfprev_domain_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
-
-  provider = aws.aws-us
-}
+//  provider = aws.aws-us
+//}
