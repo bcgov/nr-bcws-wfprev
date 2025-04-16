db2a732d8539faa4d6d81fe7da5d761ea96aa652
Sean Sylver <ssylver@VividSolutions.com>
2025-04-16 13:38:56 -0700

    revert

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,8 @@
-# resource "aws_cloudwatch_log_group" "api_logs" {
-#   name              = "/aws/apigateway/${aws_apigatewayv2_stage.wfprev_stage.name}"
-#   retention_in_days = 90
-# }
+resource "aws_cloudwatch_log_group" "api_logs" {
+  name              = "/aws/apigateway/${aws_apigatewayv2_stage.wfprev_stage.name}"
+  retention_in_days = 90
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
