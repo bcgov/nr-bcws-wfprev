2610dc70c9c20845ba6a873691d62a87f44c3a1f
Sean Sylver <ssylver@VividSolutions.com>
2025-04-16 12:34:17 -0700

    simplify lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,38 @@
-resource "aws_lambda_function" "gdb_extractor" {
-  function_name = "wfprev-gdb-extractor"
+# Lambda execution role
+resource "aws_iam_role" "lambda_role" {
+  name = "wfprev-${var.TARGET_ENV}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Lambda basic execution policy (minimum required for Lambda to run)
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda function using container image
+resource "aws_lambda_function" "gdb_processor" {
+  function_name = "wfprev-${var.TARGET_ENV}"
+  role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
+  
+  # Corrected image URI
   image_uri     = "ghcr.io/bcgov/nr-bcws-wfprev-wfprev-gdb-extractor:latest"
-  role          = "arn:aws:iam::183631341627:role/github-actions-role"
-  timeout       = 30
-  memory_size   = 512
-  publish       = true
+
+  memory_size   = var.WFPREV_LAMBDA_MEMORY
+  timeout       = var.WFPREV_LAMBDA_TIMEOUT
 
   environment {
     variables = {
