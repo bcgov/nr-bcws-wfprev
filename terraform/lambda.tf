0c7d17698438a4b82fde872f4e20717ff70fe379
Sean Sylver <ssylver@VividSolutions.com>
2025-04-16 11:15:25 -0700

    Add prevent_destroy

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -74,6 +74,9 @@ resource "aws_lambda_function" "gdb_processor" {
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/${aws_lambda_function.gdb_processor.function_name}"
   retention_in_days = 30
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # API Gateway
