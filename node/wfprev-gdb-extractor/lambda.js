7498056f0904704e7f3e0bfb4bb9fdffd99e5c8f
Sean Sylver <ssylver@VividSolutions.com>
2025-04-23 09:38:11 -0700

    Update lambda.js for HTTP API v2

--- node/wfprev-gdb-extractor/lambda.js
+++ node/wfprev-gdb-extractor/lambda.js
@@ -9,14 +9,21 @@ const binaryMimeTypes = [
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
   console.log("=== Incoming Lambda Event ===");
   console.log("HTTP Method:", event.requestContext?.http?.method);
-  console.log("Raw Path:", event.rawPath || event.path);
+  console.log("Raw Path:", event.rawPath);
   console.log("Headers:", JSON.stringify(event.headers, null, 2));
   console.log("Is Base64 Encoded:", event.isBase64Encoded);
   console.log("Body (truncated):", event.body?.slice(0, 300));
   console.log("============================");
+
+  // Map the HTTP API v2 event to something Express can understand
+  if (event.requestContext && event.requestContext.http) {
+    // For HTTP API v2
+    event.httpMethod = event.requestContext.http.method;
+    event.path = event.rawPath;
+  }
 
   return awsServerlessExpress.proxy(server, event, context);
 };
