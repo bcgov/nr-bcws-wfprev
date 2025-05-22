c9395658bdd02910922fefaaa7ce50e790c5510b
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2025-05-22 09:30:21 -0700

    WFPREV-272 UI changes (#662)

--- client/wfprev-war/src/main/angular/src/app/components/edit-project.module.ts
+++ client/wfprev-war/src/main/angular/src/app/components/edit-project.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EditProjectComponent } from 'src/app/components/edit-project/edit-project.component';
-import { CanDeactivateGuard } from 'src/app/services/util/can-deactive.guard';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: EditProjectComponent,
-    canDeactivate: [CanDeactivateGuard]
-  }
-];
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { EditProjectRoutingModule } from './edit-project-routing.module';
+import { EditProjectComponent } from './edit-project/edit-project.component';
+import { ProjectDetailsComponent } from './edit-project/project-details/project-details.component';
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  providers: [CanDeactivateGuard]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
+    TextFieldModule,
+    EditProjectRoutingModule,
+    EditProjectComponent,
+    ProjectDetailsComponent 
+  ]
 })
 export class EditProjectModule {}
