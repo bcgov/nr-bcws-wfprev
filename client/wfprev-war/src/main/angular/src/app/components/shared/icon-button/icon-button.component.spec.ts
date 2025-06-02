ea0674a6366600a03afebac98fef09a18ea2922c
Lucas Li <yzlucas@outlook.com>
2025-06-02 14:18:14 -0700

    more tests

---
+++ client/wfprev-war/src/main/angular/src/app/components/shared/icon-button/icon-button.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IconButtonComponent } from './icon-button.component';
+import { By } from '@angular/platform-browser';
+
+describe('IconButtonComponent', () => {
+  let component: IconButtonComponent;
+  let fixture: ComponentFixture<IconButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IconButtonComponent] // standalone component
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(IconButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should emit clicked event when button is clicked and not disabled', () => {
+    spyOn(component.clicked, 'emit');
+    component.disabled = false;
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', new MouseEvent('click'));
+
+    expect(component.clicked.emit).toHaveBeenCalled();
+  });
+});
