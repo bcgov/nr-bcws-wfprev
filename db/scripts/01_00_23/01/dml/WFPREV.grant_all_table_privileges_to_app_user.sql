21a1f5bb8955fbd489a0e38e165bbda3b91e126a
Lucas Li <yzlucas@outlook.com>
2025-06-25 15:43:47 -0700

    test

--- db/scripts/01_00_23/01/dml/WFPREV.grant_all_table_privileges_to_app_user.sql
+++ db/scripts/01_00_23/01/dml/WFPREV.grant_all_table_privileges_to_app_user.sql
@@ -1,6 +1,23 @@
-GRANT USAGE ON SCHEMA wfprev TO app_wf1_prev;
+-- Ensure the role can use the schema
+GRANT USAGE ON SCHEMA wfprev TO app_wf1_prev_rest_proxy;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wfprev TO app_wf1_prev;
+-- Grant all necessary privileges on existing sequences
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA wfprev TO app_wf1_prev_rest_proxy;
 
+-- Grant all necessary privileges on existing tables
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wfprev TO app_wf1_prev_rest_proxy;
+
+-- Grant all necessary privileges on future tables in this schema
 ALTER DEFAULT PRIVILEGES IN SCHEMA wfprev
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_wf1_prev;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_wf1_prev_rest_proxy;
+
+-- Grant select on future sequences in this schema
+ALTER DEFAULT PRIVILEGES IN SCHEMA wfprev
+GRANT SELECT ON SEQUENCES TO app_wf1_prev_rest_proxy;
+
+-- (Optional) Grant execute on functions in case any stored procs or functions are added
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA wfprev TO app_wf1_prev_rest_proxy;
+
+-- (Optional) For future functions
+ALTER DEFAULT PRIVILEGES IN SCHEMA wfprev
+GRANT EXECUTE ON FUNCTIONS TO app_wf1_prev_rest_proxy;
