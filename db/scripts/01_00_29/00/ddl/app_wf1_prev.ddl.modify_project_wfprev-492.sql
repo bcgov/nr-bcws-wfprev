c8d6d04cae48a070725e992048b873ed9440b048
Henry Saldyga <hsaldyga@vividsolutions.com>
2025-07-23 13:49:33 -0700

    WFPREV-598, 606

---
+++ db/scripts/01_00_29/00/ddl/app_wf1_prev.ddl.modify_project_wfprev-492.sql
@@ -0,0 +1,5 @@
+alter table project alter column secondary_objective_rationale type varchar(1000);
+alter table project_audit alter column secondary_objective_rationale type varchar(1000);
+
+alter table project alter column tertiary_objective_rationale type varchar(1000);
+alter table project_audit alter column tertiary_objective_rationale type varchar(1000);
