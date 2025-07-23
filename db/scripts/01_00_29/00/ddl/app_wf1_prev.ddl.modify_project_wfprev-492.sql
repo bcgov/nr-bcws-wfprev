4da05e6914bec205fa9dfa9c8a75895143386e93
Henry Saldyga <hsaldyga@vividsolutions.com>
2025-07-23 14:25:24 -0700

    fixes for WFPREV-606

--- db/scripts/01_00_29/00/ddl/app_wf1_prev.ddl.modify_project_wfprev-492.sql
+++ db/scripts/01_00_29/00/ddl/app_wf1_prev.ddl.modify_project_wfprev-492.sql
@@ -1,5 +1,5 @@
-alter table project alter column secondary_objective_rationale type varchar(1000);
-alter table project_audit alter column secondary_objective_rationale type varchar(1000);
+alter table wfprev.project alter column secondary_objective_rationale type varchar(1000);
+alter table wfprev.project_audit alter column secondary_objective_rationale type varchar(1000);
 
-alter table project alter column tertiary_objective_rationale type varchar(1000);
-alter table project_audit alter column tertiary_objective_rationale type varchar(1000);
+alter table wfprev.project alter column tertiary_objective_rationale type varchar(1000);
+alter table wfprev.project_audit alter column tertiary_objective_rationale type varchar(1000);
