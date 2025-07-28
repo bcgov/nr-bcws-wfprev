f8e4dfedf0e602faf335d554d2f20f877900b410
Lucas Li <yzlucas@outlook.com>
2025-07-27 23:28:08 -0700

    wfprev 624

--- client/wfprev-war/src/main/angular/src/app/utils/constants.ts
+++ client/wfprev-war/src/main/angular/src/app/utils/constants.ts
@@ -22,10 +22,20 @@ export const Messages = {
   activityDeletedFailure: 'Activity Deleted Failed',
   fileUploadSuccess: 'File Uploaded Successfully',
   fileUploadFailure: 'File Uploaded Failed',
+  fileUploadInProgress: 'File upload in progress, Please wait',
   evaluationCriteriaCreatedSuccess: 'Evaluation Criteria Created Successfully.',
   evaluationCriteriaCreatedFailure: 'Failed to Create Evaluation Criteria.',
   evaluationCriteriaUpdatedSuccess: 'Evaluation Criteria Updated Successfully.',
   evaluationCriteriaUpdatedFailure: 'Failed to Update Evaluation Criteria.',
+  fileDownloadFailure: 'Error: File download unsuccessful',
+  fileDownloadSuccess: 'File downloaded successfully.',
+  fileDownloadInProgress: 'File download in progress. Please wait',
+  csvFileDownloadFailure: 'Error: CSV File download unsuccessful',
+  excelFileDownloadFailure: 'Error: Excel File download unsuccessful',
+  csvFileDownloadInProgress: 'CSV File download in progress. Please wait',
+  csvFileDownloadSuccess: 'CSV File downloaded successfully',
+  excelFileDownloadInProgress: 'Excel File download in progress. Please wait',
+  excelFileDownloadSuccess: 'Excel File downloaded successfully',
 };
 
 export const CodeTableKeys = {
