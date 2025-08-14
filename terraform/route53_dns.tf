1a0e8d4d2047b2988f81544bd30e3ee074e75a79
Chris Preston <cpreston@vividsolutions.com>
2025-08-14 13:30:14 -0700

    Use automatic value instead of manually specifying URL

--- terraform/route53_dns.tf
+++ terraform/route53_dns.tf
@@ -38,7 +38,7 @@ resource "aws_route53_record" "cert_validation_record_ca" {
 
 resource "aws_route53_record" "wfprev_vanity_url" {
   zone_id = data.aws_route53_zone.wfprev_route53_zone.id
-  name    = "${var.client_name}.${var.SHORTENED_ENV}.${var.gov_domain}"
+  name    = data.aws_route53_zone.wfprev_route53_zone.name
   type    = "A"
   alias {
     name                   = aws_cloudfront_distribution.wfprev_app_distribution.domain_name
