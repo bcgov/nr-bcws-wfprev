a55be18eaf3ff8745f14873714fb6751215394f3
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2025-10-02 16:40:14 -0700

    Update FeaturesServiceTest.java

--- server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/services/FeaturesServiceTest.java
+++ server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/services/FeaturesServiceTest.java
@@ -206,38 +206,15 @@ class FeaturesServiceTest {
     @Test
     void testFindFilteredProjects() {
         FeatureQueryParams params = new FeatureQueryParams();
-        params.setProgramAreaGuids(List.of(UUID.randomUUID()));
 
         when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
-
-        CriteriaQuery<UUID> uuidQuery = mock(CriteriaQuery.class);
-        Root<ProjectEntity> idRoot = mock(Root.class);
-        Path<Object> mockPath = mock(Path.class);
-        Predicate mockPredicate = mock(Predicate.class);
-
-        when(criteriaBuilder.createQuery(UUID.class)).thenReturn(uuidQuery);
-        when(uuidQuery.from(ProjectEntity.class)).thenReturn(idRoot);
-        when(uuidQuery.select(any())).thenReturn(uuidQuery);
-
-        when(idRoot.get("programAreaGuid")).thenReturn(mockPath);
-        when(mockPath.in(anyCollection())).thenReturn(mockPredicate);
-
-        TypedQuery<UUID> mockUuidQuery = mock(TypedQuery.class);
-        when(entityManager.createQuery(uuidQuery)).thenReturn(mockUuidQuery);
-        when(mockUuidQuery.setFirstResult(anyInt())).thenReturn(mockUuidQuery);
-        when(mockUuidQuery.setMaxResults(anyInt())).thenReturn(mockUuidQuery);
-        when(mockUuidQuery.getResultList()).thenReturn(Collections.singletonList(UUID.randomUUID()));
-
         when(criteriaBuilder.createQuery(ProjectEntity.class)).thenReturn(projectQuery);
         when(projectQuery.from(ProjectEntity.class)).thenReturn(projectRoot);
-        when(projectQuery.where(any(Predicate.class))).thenReturn(projectQuery);
-        when(projectQuery.distinct(true)).thenReturn(projectQuery);
+        TypedQuery<ProjectEntity> mockQuery = mock(TypedQuery.class);
+        when(entityManager.createQuery(projectQuery)).thenReturn(mockQuery);
+        when(mockQuery.getResultList()).thenReturn(Collections.singletonList(new ProjectEntity()));
 
-        TypedQuery<ProjectEntity> mockProjectQuery = mock(TypedQuery.class);
-        when(entityManager.createQuery(projectQuery)).thenReturn(mockProjectQuery);
-        when(mockProjectQuery.getResultList()).thenReturn(Collections.singletonList(new ProjectEntity()));
-
-        List<ProjectEntity> result = featuresService.findFilteredProjects(params, 1, 20);
+        List<ProjectEntity> result = featuresService.findFilteredProjects(params,1,20);
 
         assertNotNull(result);
         assertEquals(1, result.size());
