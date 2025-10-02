8063549c57e8d0814d287bce89b73503e57dd42d
Sean Sylver <ssylver@vividsolutions.com>
2025-10-02 13:46:22 -0700

    WFPREV-813 project location endpoint

--- server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/services/ProjectLocationServiceTest.java
+++ server/wfprev-api/src/test/java/ca/bc/gov/nrs/wfprev/services/ProjectLocationServiceTest.java
@@ -15,9 +15,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 class ProjectLocationServiceTest {
 
