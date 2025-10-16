53dac8629be964f9dcee21575ad254a70f2fc270
Lucas Li <35748253+yzlucas@users.noreply.github.com>
2025-10-16 16:36:30 -0700

    Update token.service.ts

--- client/wfprev-war/src/main/angular/src/app/services/token.service.ts
+++ client/wfprev-war/src/main/angular/src/app/services/token.service.ts
@@ -188,7 +188,7 @@ export class TokenService {
     const authConfig = {
       oidc: false,
       issuer: configuration.application.baseUrl,
-      loginUrl: configuration.webade.oauth2Url,
+      loginUrl: "https://wfappsi.nrs.gov.bc.ca/ext/oauth2/v1/oauth/authorize"
       redirectUri: redirectUri ?? globalThis.location?.href,
       clientId: configuration.webade.clientId,
       scope: configuration.webade.authScopes
