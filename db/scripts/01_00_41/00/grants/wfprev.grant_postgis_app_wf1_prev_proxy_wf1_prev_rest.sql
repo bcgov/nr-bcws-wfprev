b3f3c7d136df1c1f684d296e5770c8c71a23c577
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2025-10-31 17:07:29 -0700

    WFPREV-822 install postgis for app_wf1_prev and proxy_wf1_prev_rest users (#1056)

---
+++ db/scripts/01_00_41/00/grants/wfprev.grant_postgis_app_wf1_prev_proxy_wf1_prev_rest.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA public TO app_wf1_prev, proxy_wf1_prev_rest;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO app_wf1_prev, proxy_wf1_prev_rest;
+GRANT ALL ON TABLE public.spatial_ref_sys TO app_wf1_prev, proxy_wf1_prev_rest;
