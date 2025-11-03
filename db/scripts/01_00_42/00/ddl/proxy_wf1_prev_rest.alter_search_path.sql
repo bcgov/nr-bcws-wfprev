d13187c5e95b06b6a81ed4ac5be8b7d406450e57
ssylver93 <107515688+ssylver93@users.noreply.github.com>
2025-11-03 15:43:59 -0800

    WFPREV-822 update alter role syntax and execute using superuser (#1061)

--- db/scripts/01_00_42/00/ddl/proxy_wf1_prev_rest.alter_search_path.sql
+++ db/scripts/01_00_42/00/ddl/proxy_wf1_prev_rest.alter_search_path.sql
@@ -1,3 +1,3 @@
 -- Add public to proxy_wf1_prev_rest's search_path so PostGIS functions in public schema resolve correctly
-ALTER ROLE proxy_wf1_prev_rest IN DATABASE '${WFPREV_DATABASE_NAME}'
+ALTER ROLE proxy_wf1_prev_rest IN DATABASE ${WFPREV_DATABASE_NAME}
 SET search_path = wfprev, public;
