e700e33accfe624a7d6b57304f70f40aff9cbba6
Sukh <6563909+sukhpalp@users.noreply.github.com>
2025-11-04 17:06:05 -0800

    Revert "Revert breaking changes for wfprev-changelog checksums" (#1065)

--- db/scripts/01_00_00/00/ddl/logins/wfprev.ddl.create_login_PROXY_WF1_PREV_REST.sql
+++ db/scripts/01_00_00/00/ddl/logins/wfprev.ddl.create_login_PROXY_WF1_PREV_REST.sql
@@ -1,13 +1,13 @@
 -- Role: "proxy_wf1_prev_rest"
 -- DROP ROLE "proxy_wf1_prev_rest";
 
-CREATE ROLE "proxy_wf1_prev_rest" WITH
+CREATE ROLE proxy_wf1_prev_rest WITH
   LOGIN
   NOSUPERUSER
   INHERIT
   NOCREATEDB
   NOCREATEROLE
   NOREPLICATION
-  PASSWORD "${PROXY_WF1_PREV_REST_PASSWORD}";
+  PASSWORD '${PROXY_WF1_PREV_REST_PASSWORD}';
 
 COMMENT ON ROLE "proxy_wf1_prev_rest" IS 'Proxy account for Wildfire Prevention System.';
