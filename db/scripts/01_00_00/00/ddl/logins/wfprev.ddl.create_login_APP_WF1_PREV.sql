ccbdc034b116e7c58a09fb874da5baf92e975d59
Sukh <6563909+sukhpalp@users.noreply.github.com>
2025-11-04 17:03:46 -0800

    Revert "Revert breaking changes for wfprev-changelog checksums (#1062)"
    This reverts commit 77f488bbaefee2abbf8c8761caa320f1a5826878.

--- db/scripts/01_00_00/00/ddl/logins/wfprev.ddl.create_login_APP_WF1_PREV.sql
+++ db/scripts/01_00_00/00/ddl/logins/wfprev.ddl.create_login_APP_WF1_PREV.sql
@@ -1,13 +1,13 @@
 -- Role: "app_wf1_prev"
 -- DROP ROLE "app_wf1_prev";
 
-CREATE ROLE "app_wf1_prev" WITH
+CREATE ROLE app_wf1_prev WITH
   LOGIN
   NOSUPERUSER
   INHERIT
   NOCREATEDB
   NOCREATEROLE
   NOREPLICATION
-  PASSWORD "${APP_WF1_PREV_PASSWORD}"
+  PASSWORD '${APP_WF1_PREV_PASSWORD}';
 
 COMMENT ON ROLE "app_wf1_prev" IS 'Wildfire Prevention System.';
