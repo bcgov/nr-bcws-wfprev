5f8812536ea46e0f30362c99ff12b86e1f343202
Sukh <6563909+sukhpalp@users.noreply.github.com>
2026-01-27 10:12:53 -0800

    WFPREV-869 : Implement report columns for performance upgrades (#1082)

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/entities/CulturalPrescribedFireReportEntity.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/entities/CulturalPrescribedFireReportEntity.java
@@ -185,4 +185,215 @@ public class CulturalPrescribedFireReportEntity implements Serializable {
 
     @Column(name = "total_filter_section_score")
     private BigDecimal totalFilterSectionScore;
+
+    // Performance Update Fields
+    @Column(name = "q1_submitted_timestamp")
+    private Date q1SubmittedTimestamp;
+
+    @Column(name = "q1_general_update_comment")
+    private String q1GeneralUpdateComment;
+
+    @Column(name = "q1_progress_status_code")
+    private String q1ProgressStatusCode;
+
+    @Column(name = "q1_forecast_amount")
+    private BigDecimal q1ForecastAmount;
+
+    @Column(name = "q1_forecast_adjustment_amount")
+    private BigDecimal q1ForecastAdjustmentAmount;
+
+    @Column(name = "q1_forecast_adjustment_rationale")
+    private String q1ForecastAdjustmentRationale;
+
+    @Column(name = "q1_budget_high_risk_amount")
+    private BigDecimal q1BudgetHighRiskAmount;
+
+    @Column(name = "q1_budget_high_risk_rationale")
+    private String q1BudgetHighRiskRationale;
+
+    @Column(name = "q1_budget_medium_risk_amount")
+    private BigDecimal q1BudgetMediumRiskAmount;
+
+    @Column(name = "q1_budget_medium_risk_rationale")
+    private String q1BudgetMediumRiskRationale;
+
+    @Column(name = "q1_budget_low_risk_amount")
+    private BigDecimal q1BudgetLowRiskAmount;
+
+    @Column(name = "q1_budget_low_risk_rationale")
+    private String q1BudgetLowRiskRationale;
+
+    @Column(name = "q1_budget_completed_amount")
+    private BigDecimal q1BudgetCompletedAmount;
+
+    @Column(name = "q1_budget_completed_description")
+    private String q1BudgetCompletedDescription;
+
+    @Column(name = "q2_submitted_timestamp")
+    private Date q2SubmittedTimestamp;
+
+    @Column(name = "q2_general_update_comment")
+    private String q2GeneralUpdateComment;
+
+    @Column(name = "q2_progress_status_code")
+    private String q2ProgressStatusCode;
+
+    @Column(name = "q2_forecast_amount")
+    private BigDecimal q2ForecastAmount;
+
+    @Column(name = "q2_forecast_adjustment_amount")
+    private BigDecimal q2ForecastAdjustmentAmount;
+
+    @Column(name = "q2_forecast_adjustment_rationale")
+    private String q2ForecastAdjustmentRationale;
+
+    @Column(name = "q2_budget_high_risk_amount")
+    private BigDecimal q2BudgetHighRiskAmount;
+
+    @Column(name = "q2_budget_high_risk_rationale")
+    private String q2BudgetHighRiskRationale;
+
+    @Column(name = "q2_budget_medium_risk_amount")
+    private BigDecimal q2BudgetMediumRiskAmount;
+
+    @Column(name = "q2_budget_medium_risk_rationale")
+    private String q2BudgetMediumRiskRationale;
+
+    @Column(name = "q2_budget_low_risk_amount")
+    private BigDecimal q2BudgetLowRiskAmount;
+
+    @Column(name = "q2_budget_low_risk_rationale")
+    private String q2BudgetLowRiskRationale;
+
+    @Column(name = "q2_budget_completed_amount")
+    private BigDecimal q2BudgetCompletedAmount;
+
+    @Column(name = "q2_budget_completed_description")
+    private String q2BudgetCompletedDescription;
+
+    @Column(name = "q3_submitted_timestamp")
+    private Date q3SubmittedTimestamp;
+
+    @Column(name = "q3_general_update_comment")
+    private String q3GeneralUpdateComment;
+
+    @Column(name = "q3_progress_status_code")
+    private String q3ProgressStatusCode;
+
+    @Column(name = "q3_forecast_amount")
+    private BigDecimal q3ForecastAmount;
+
+    @Column(name = "q3_forecast_adjustment_amount")
+    private BigDecimal q3ForecastAdjustmentAmount;
+
+    @Column(name = "q3_forecast_adjustment_rationale")
+    private String q3ForecastAdjustmentRationale;
+
+    @Column(name = "q3_budget_high_risk_amount")
+    private BigDecimal q3BudgetHighRiskAmount;
+
+    @Column(name = "q3_budget_high_risk_rationale")
+    private String q3BudgetHighRiskRationale;
+
+    @Column(name = "q3_budget_medium_risk_amount")
+    private BigDecimal q3BudgetMediumRiskAmount;
+
+    @Column(name = "q3_budget_medium_risk_rationale")
+    private String q3BudgetMediumRiskRationale;
+
+    @Column(name = "q3_budget_low_risk_amount")
+    private BigDecimal q3BudgetLowRiskAmount;
+
+    @Column(name = "q3_budget_low_risk_rationale")
+    private String q3BudgetLowRiskRationale;
+
+    @Column(name = "q3_budget_completed_amount")
+    private BigDecimal q3BudgetCompletedAmount;
+
+    @Column(name = "q3_budget_completed_description")
+    private String q3BudgetCompletedDescription;
+
+    @Column(name = "march7_submitted_timestamp")
+    private Date march7SubmittedTimestamp;
+
+    @Column(name = "march7_general_update_comment")
+    private String march7GeneralUpdateComment;
+
+    @Column(name = "march7_progress_status_code")
+    private String march7ProgressStatusCode;
+
+    @Column(name = "march7_forecast_amount")
+    private BigDecimal march7ForecastAmount;
+
+    @Column(name = "march7_forecast_adjustment_amount")
+    private BigDecimal march7ForecastAdjustmentAmount;
+
+    @Column(name = "march7_forecast_adjustment_rationale")
+    private String march7ForecastAdjustmentRationale;
+
+    @Column(name = "march7_budget_high_risk_amount")
+    private BigDecimal march7BudgetHighRiskAmount;
+
+    @Column(name = "march7_budget_high_risk_rationale")
+    private String march7BudgetHighRiskRationale;
+
+    @Column(name = "march7_budget_medium_risk_amount")
+    private BigDecimal march7BudgetMediumRiskAmount;
+
+    @Column(name = "march7_budget_medium_risk_rationale")
+    private String march7BudgetMediumRiskRationale;
+
+    @Column(name = "march7_budget_low_risk_amount")
+    private BigDecimal march7BudgetLowRiskAmount;
+
+    @Column(name = "march7_budget_low_risk_rationale")
+    private String march7BudgetLowRiskRationale;
+
+    @Column(name = "march7_budget_completed_amount")
+    private BigDecimal march7BudgetCompletedAmount;
+
+    @Column(name = "march7_budget_completed_description")
+    private String march7BudgetCompletedDescription;
+
+    @Column(name = "other_submitted_timestamp")
+    private Date otherSubmittedTimestamp;
+
+    @Column(name = "other_general_update_comment")
+    private String otherGeneralUpdateComment;
+
+    @Column(name = "other_progress_status_code")
+    private String otherProgressStatusCode;
+
+    @Column(name = "other_forecast_amount")
+    private BigDecimal otherForecastAmount;
+
+    @Column(name = "other_forecast_adjustment_amount")
+    private BigDecimal otherForecastAdjustmentAmount;
+
+    @Column(name = "other_forecast_adjustment_rationale")
+    private String otherForecastAdjustmentRationale;
+
+    @Column(name = "other_budget_high_risk_amount")
+    private BigDecimal otherBudgetHighRiskAmount;
+
+    @Column(name = "other_budget_high_risk_rationale")
+    private String otherBudgetHighRiskRationale;
+
+    @Column(name = "other_budget_medium_risk_amount")
+    private BigDecimal otherBudgetMediumRiskAmount;
+
+    @Column(name = "other_budget_medium_risk_rationale")
+    private String otherBudgetMediumRiskRationale;
+
+    @Column(name = "other_budget_low_risk_amount")
+    private BigDecimal otherBudgetLowRiskAmount;
+
+    @Column(name = "other_budget_low_risk_rationale")
+    private String otherBudgetLowRiskRationale;
+
+    @Column(name = "other_budget_completed_amount")
+    private BigDecimal otherBudgetCompletedAmount;
+
+    @Column(name = "other_budget_completed_description")
+    private String otherBudgetCompletedDescription;
 }
