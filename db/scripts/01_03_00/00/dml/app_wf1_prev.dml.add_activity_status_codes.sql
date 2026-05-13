59ae1ca4e84110a925dd5e9ee9d85b7ac818b3d3
Sean Sylver <ssylver@vividsolutions.com>
2026-05-13 14:09:24 -0700

    WFPREV-1031 Add ActivityStatusCodes

---
+++ db/scripts/01_03_00/00/dml/app_wf1_prev.dml.add_activity_status_codes.sql
@@ -0,0 +1,9 @@
+insert into wfprev.activity_status_code (activity_status_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('SUBS_COMPL', 'Substantially Complete', 5, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'));
+insert into wfprev.activity_status_code (activity_status_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('IN_PROG', 'In Progress', 6, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'));
+insert into wfprev.activity_status_code (activity_status_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('DEFERRED', 'Deferred', 7, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'));
+insert into wfprev.activity_status_code (activity_status_code, description, display_order, effective_date, expiry_date, revision_count, create_user, create_date, update_user, update_date)
+values ('CANCELLED', 'Cancelled', 8, to_date('01-01-2000', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 0, 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'), 'DATA_LOAD', to_date('13-05-2026', 'dd-mm-yyyy'));
+
