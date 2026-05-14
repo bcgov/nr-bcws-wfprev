ab7d0e1eeb7f46bdb3614031f967d849b28bbfe3
Sukhpal <sparmar@vividsolutions.com>
2026-05-14 15:07:50 -0700

    WFPREV-1030 : Fiscal Close Out table

---
+++ db/scripts/01_03_00/01/grants/wfprev.grants.project_plan_fiscal_close_out.sql
@@ -0,0 +1,6 @@
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out TO app_wf1_prev_rest_proxy;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out_audit TO app_wf1_prev_rest_proxy;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out TO proxy_wf1_prev_rest;
+GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out_audit TO proxy_wf1_prev_rest;
