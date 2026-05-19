8038d7cc12ab62b4224c4b7fff7db6af861108cd
Sukhpal <sparmar@vividsolutions.com>
2026-05-19 12:06:29 -0700

    Fix db scripts

--- db/scripts/01_03_00/01/grants/wfprev.grants.project_plan_fiscal_close_out.sql
+++
@@ -1,6 +0,0 @@
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out TO app_wf1_prev_rest_proxy;
-GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out_audit TO app_wf1_prev_rest_proxy;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out TO proxy_wf1_prev_rest;
-GRANT SELECT, INSERT, UPDATE, DELETE ON wfprev.project_plan_fiscal_close_out_audit TO proxy_wf1_prev_rest;
