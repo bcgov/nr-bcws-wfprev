c4e5967a4386cfb66e15f9f842e3de50f9e7c88e
Sean Sylver <ssylver@vividsolutions.com>
2026-06-22 10:11:14 -0700

    Remove unused imports

--- server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/models/ProjectFiscalModel.java
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/models/ProjectFiscalModel.java
@@ -1,22 +1,18 @@
 package ca.bc.gov.nrs.wfprev.data.models;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.hateoas.server.core.Relation;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import ca.bc.gov.nrs.wfprev.common.entities.CommonModel;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.server.core.Relation;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
