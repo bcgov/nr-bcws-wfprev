c71dabfbc8ec742c0316cd601b8335812fcf913f
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:33:39 -0700

    WFPREV-1126: Add Opening Category to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -15,7 +15,8 @@ SELECT p.project_name,
        p.project_lead_email_address,
        p.results_project_code,
        ppf.results_opening_id,
-       NULL AS results_opening_action
+       NULL AS results_opening_action,
+       NULL AS results_opening_category
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
