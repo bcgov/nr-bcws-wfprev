7e70a77c6f30081351b7e00db2fa67c182bee458
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:31:21 -0700

    WFPREV-1126: Add Opening ID to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -13,7 +13,8 @@ SELECT p.project_name,
        END AS fiscal_year,
        fdou.org_unit_name AS forest_district_name,
        p.project_lead_email_address,
-       p.results_project_code
+       p.results_project_code,
+       ppf.results_opening_id
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
