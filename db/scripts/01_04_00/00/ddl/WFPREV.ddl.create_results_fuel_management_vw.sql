82a3105ed4dc6bf9fbc4c34d8599b80cd1a12517
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:01:43 -0700

    WFPREV-1126: Add Org Unit to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -10,8 +10,10 @@ SELECT p.project_name,
        CASE
          WHEN ppf.fiscal_year IS NOT NULL
          THEN ppf.fiscal_year::int::text || '/' || right((ppf.fiscal_year::int + 1)::text, 2)
-       END AS fiscal_year
+       END AS fiscal_year,
+       fdou.org_unit_name AS forest_district_name
 FROM wfprev.project p
+  LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
   LEFT JOIN wfprev.activity a              ON a.project_plan_fiscal_guid = ppf.project_plan_fiscal_guid
 WHERE p.project_type_code = 'FUEL_MGMT';
