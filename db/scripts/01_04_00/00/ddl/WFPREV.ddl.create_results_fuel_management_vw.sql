5beecbb2fad18b362012fbdc5f448ed62035627e
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 13:38:55 -0700

    WFPREV-1126: Add Activity Name to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -1,5 +1,7 @@
 drop view if exists wfprev.results_fuel_management_vw;
 
 CREATE or REPLACE VIEW wfprev.results_fuel_management_vw AS
-select ppf.project_fiscal_name
-from wfprev.project_plan_fiscal ppf;
+select ppf.project_fiscal_name,
+       a.activity_name
+from wfprev.project_plan_fiscal ppf
+left join wfprev.activity a on a.project_plan_fiscal_guid = ppf.project_plan_fiscal_guid;
