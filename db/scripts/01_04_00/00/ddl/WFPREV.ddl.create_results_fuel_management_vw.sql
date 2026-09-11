093c4ff3db5208e16b0d9f90759946bab4c175b8
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 13:54:45 -0700

    WFPREV-1126: Add Activity Status to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -5,7 +5,8 @@ SELECT p.project_name,
        ppf.project_fiscal_name,
        a.activity_name,
        CASE WHEN a.is_results_reportable_ind THEN 'Y' ELSE 'N' END AS is_results_reportable_ind,
-       a.activity_description
+       a.activity_description,
+       a.activity_status_code
 FROM wfprev.project p
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
   LEFT JOIN wfprev.activity a              ON a.project_plan_fiscal_guid = ppf.project_plan_fiscal_guid
