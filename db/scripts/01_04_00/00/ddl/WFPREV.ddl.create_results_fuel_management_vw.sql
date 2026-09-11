3e656208e2d39784c8258479583d07c22e93d803
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:32:50 -0700

    WFPREV-1126: Add Opening Action to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -14,7 +14,8 @@ SELECT p.project_name,
        fdou.org_unit_name AS forest_district_name,
        p.project_lead_email_address,
        p.results_project_code,
-       ppf.results_opening_id
+       ppf.results_opening_id,
+       NULL AS results_opening_action
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
