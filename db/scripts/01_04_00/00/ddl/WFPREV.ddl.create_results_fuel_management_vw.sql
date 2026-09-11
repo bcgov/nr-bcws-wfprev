a6025f782980c1bea99e41f3b63745ed09be4318
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 13:52:34 -0700

    WFPREV-1126: Add Activity Description to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -4,7 +4,8 @@ CREATE OR REPLACE VIEW wfprev.results_fuel_management_vw AS
 SELECT p.project_name,
        ppf.project_fiscal_name,
        a.activity_name,
-       CASE WHEN a.is_results_reportable_ind THEN 'Y' ELSE 'N' END AS is_results_reportable_ind
+       CASE WHEN a.is_results_reportable_ind THEN 'Y' ELSE 'N' END AS is_results_reportable_ind,
+       a.activity_description
 FROM wfprev.project p
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
   LEFT JOIN wfprev.activity a              ON a.project_plan_fiscal_guid = ppf.project_plan_fiscal_guid
