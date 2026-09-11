09f41c9250642767a1f256be815fb00689b50dde
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:05:59 -0700

    WFPREV-1126: Add Inter-Agency No. Licensee Opening ID to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -12,7 +12,8 @@ SELECT p.project_name,
          THEN ppf.fiscal_year::int::text || '/' || right((ppf.fiscal_year::int + 1)::text, 2)
        END AS fiscal_year,
        fdou.org_unit_name AS forest_district_name,
-       p.project_lead_email_address
+       p.project_lead_email_address,
+       p.results_project_code
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
