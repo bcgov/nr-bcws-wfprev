c0b92acab23bdc88ab0bca5f2641907f7cee6812
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 13:44:02 -0700

    WFPREV-1126: Add RESULTS Reportable (Y/N) to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -2,6 +2,7 @@ drop view if exists wfprev.results_fuel_management_vw;
 
 CREATE or REPLACE VIEW wfprev.results_fuel_management_vw AS
 select ppf.project_fiscal_name,
-       a.activity_name
+       a.activity_name,
+       case when a.is_results_reportable_ind then 'Y' else 'N' end as is_results_reportable_ind
 from wfprev.project_plan_fiscal ppf
 left join wfprev.activity a on a.project_plan_fiscal_guid = ppf.project_plan_fiscal_guid;
