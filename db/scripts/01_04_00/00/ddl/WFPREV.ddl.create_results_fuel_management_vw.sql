526cb7b9b99ae02d8bbd794cc67262972fcd2bf1
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:03:33 -0700

    WFPREV-1126: Add Project Lead Email to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -11,7 +11,8 @@ SELECT p.project_name,
          WHEN ppf.fiscal_year IS NOT NULL
          THEN ppf.fiscal_year::int::text || '/' || right((ppf.fiscal_year::int + 1)::text, 2)
        END AS fiscal_year,
-       fdou.org_unit_name AS forest_district_name
+       fdou.org_unit_name AS forest_district_name,
+       p.project_lead_email_address
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
