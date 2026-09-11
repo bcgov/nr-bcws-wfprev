067763a2d0677f5cb78a7f2d32fde71474e13032
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-11 14:40:55 -0700

    WFPREV-1126: Add Max Permanent Access % to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -17,7 +17,8 @@ SELECT p.project_name,
        ppf.results_opening_id,
        NULL AS results_opening_action,
        NULL AS results_opening_category,
-       ab.boundary_size_ha
+       ab.boundary_size_ha,
+       NULL AS max_permanent_access_percent
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou    ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf ON ppf.project_guid = p.project_guid
