5a5083e5a23380cd5ebe5e4b04a76874cb475b7c
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-14 13:35:35 -0700

    WFPREV-1126: Add Forest Cover shape file name to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -39,7 +39,8 @@ SELECT p.project_name,
        CASE WHEN a.outstanding_obligations_ind THEN 'Y' ELSE 'N' END AS outstanding_obligations_ind,
        a.activity_comment,
        pfa.document_path AS opening_shape_file_name,
-       afa.document_path AS activity_shape_file_name
+       afa.document_path AS activity_shape_file_name,
+       NULL AS forest_cover_shape_file_name
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou             ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf          ON ppf.project_guid = p.project_guid
