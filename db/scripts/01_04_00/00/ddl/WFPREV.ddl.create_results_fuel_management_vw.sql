8744ce560b0a9cbab2832925fff6bf48ceba53ea
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-14 13:13:42 -0700

    WFPREV-1126: Add Outstanding Obligations to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -35,7 +35,8 @@ SELECT p.project_name,
        ppf.cfs_project_code,
        CASE WHEN a.previous_carry_forward_ind THEN 'Y' ELSE 'N' END AS previous_carry_forward_ind,
        CASE WHEN a.carry_forward_ind THEN 'Y' ELSE 'N' END AS carry_forward_ind,
-       a.final_outcome_comments
+       a.final_outcome_comments,
+       CASE WHEN a.outstanding_obligations_ind THEN 'Y' ELSE 'N' END AS outstanding_obligations_ind
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou             ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf          ON ppf.project_guid = p.project_guid
