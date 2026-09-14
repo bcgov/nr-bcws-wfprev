ce0d156e06f459f9c5632ab65358356fede26ce8
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-14 10:35:46 -0700

    WFPREV-1126: Add Secondary Objective to results_fuel_management_vw.

--- db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
+++ db/scripts/01_04_00/00/ddl/WFPREV.ddl.create_results_fuel_management_vw.sql
@@ -22,7 +22,8 @@ SELECT p.project_name,
        sbc.description AS activity_base_name,
        stc.description AS technique_name,
        smc.description AS method_name,
-       potc.description AS primary_objective_name
+       potc.description AS primary_objective_name,
+       sotc.description AS secondary_objective_name
 FROM wfprev.project p
   LEFT JOIN wfprev.forest_org_unit fdou             ON fdou.org_unit_identifier = p.forest_district_org_unit_id
   LEFT JOIN wfprev.project_plan_fiscal ppf          ON ppf.project_guid = p.project_guid
@@ -35,4 +36,5 @@ FROM wfprev.project p
   LEFT JOIN wfprev.silviculture_method sm           ON sm.silviculture_method_guid = a.silviculture_method_guid
   LEFT JOIN wfprev.silviculture_method_code smc     ON smc.silviculture_method_code = sm.silviculture_method_code
   LEFT JOIN wfprev.objective_type_code potc         ON potc.objective_type_code = p.primary_objective_type_code
+  LEFT JOIN wfprev.objective_type_code sotc         ON sotc.objective_type_code = p.secondary_objective_type_code
 WHERE p.project_type_code = 'FUEL_MGMT';
