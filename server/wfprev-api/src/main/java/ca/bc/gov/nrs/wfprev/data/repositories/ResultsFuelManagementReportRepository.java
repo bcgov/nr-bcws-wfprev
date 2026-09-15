b26c45776b38da127eb41f7298b813e534625784
Hong-Yi Wang <hwang@vividsolutions.com>
2026-09-15 14:14:56 -0700

    WFPREV-1128: Implement the repository class for wfprev.results_fuel_management_vw.

---
+++ server/wfprev-api/src/main/java/ca/bc/gov/nrs/wfprev/data/repositories/ResultsFuelManagementReportRepository.java
@@ -0,0 +1,18 @@
+package ca.bc.gov.nrs.wfprev.data.repositories;
+
+import ca.bc.gov.nrs.wfprev.data.entities.ResultsFuelManagementReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@RepositoryRestResource(exported = false)
+public interface ResultsFuelManagementReportRepository extends JpaRepository<ResultsFuelManagementReportEntity, UUID> {
+    List<ResultsFuelManagementReportEntity> findByProjectGuid(UUID projectGuid);
+
+    List<ResultsFuelManagementReportEntity> findByProjectGuidAndProjectPlanFiscalGuidIn(UUID projectGuid, Collection<UUID> fiscalGuids);
+}
